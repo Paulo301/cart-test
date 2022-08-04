738c991a6c03b8ad1a49ff0609257fabe4335400
Paulo301 <paulovictor1918@gmail.com>
2022-08-04 01:20:54 -0300

    feat: create home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
+      <Home />
       <GlobalStyle />
     </>
   );
